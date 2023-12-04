5446479b474cf5bebe23a95fa29d815302a5d688
Prabhanjan2002 <121232769+Prabhanjan2002@users.noreply.github.com>
2023-12-05 00:13:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const timeleft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
 
 let result = 0;
+let hitposition;
+let currenttime = 60;
+let timerID = null;
 
 function randomsquare() {
   squares.forEach((square) => {
@@ -12,11 +15,34 @@ function randomsquare() {
 
   let randomsquare = squares[Math.floor(Math.random() * 9)];
   randomsquare.classList.add("mole");
+
+  hitposition = randomsquare.id;
 }
 
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitposition) {
+      result++;
+      score.textContent = result;
+      hitposition = null;
+    }
+  });
+});
+
 function movemole() {
-  let timeID = null;
-  timeID = setInterval(randomsquare, 50);
+  timerID = setInterval(randomsquare, 1000);
 }
 
 movemole();
+
+function countdown() {
+  currenttime--;
+  timeleft.textContent = currenttime;
+  if (currenttime == 0) {
+    clearInterval(countdownid);
+    clearInterval(timerID);
+    alert("Game over! your score is" + result);
+  }
+}
+
+let countdownid = setInterval(countdown, 1000);
